3ab2be9d58883b77a166a97edca37863562b44bb
Kris <git@k118.de>
2020-08-22 21:02:13 +0200

    Add example

---
+++ examples/parseFromPDF.php
@@ -0,0 +1,20 @@
+<?php
+
+use REWEParser\Exception\ReceiptParseException;
+use REWEParser\Parser;
+use Spatie\PdfToText\Exceptions\PdfNotFound;
+
+require_once '../vendor/autoload.php';
+
+try {
+    $receipt = Parser::parseFromPDF('../tests/receipts/multipleProducts_multiplePaymentMethods_paybackCoupon.pdf');
+
+    echo "The receipt was made " . $receipt->getTimestamp()->diffForHumans() . ". \n";
+    echo "You've bought " . count($receipt->getPositions()) . " Products for a total of " . $receipt->getTotal() . "€. \n";
+
+} catch (ReceiptParseException $e) {
+    echo "There is something weird with the receipt... Maybe it's not compatible?\n";
+    echo "Error: " . $e->getMessage();
+} catch (PdfNotFound $e) {
+    echo "The given PDF File cannot be opened... " . $e->getMessage();
+}
